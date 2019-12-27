f8dbcca3d24f8d4bd0515a9b2153121f4e3acb3b
Nepo <59269591+nepoecruos@users.noreply.github.com>
2019-12-27 02:45:45 +0000

    Create index.php

---
+++ about/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">   
+        <title>Planet Mareep - About</title>
+        <link rel="icon" type="image/PNG" href="../assets/logo.png">
+        <link href="../style/about.css" rel="stylesheet" type="text/css">
+        <meta property="og:title" content="Planet Mareep" />
+        <meta property="og:description" content="The #1 Competitive Pok&eacute;mon community." />
+        </head>	
+        <body>
+        <header>
+            <left>
+            <h1><span>Planet Mareep</span></h1></left>
+            <nav>
+                <ul>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="../about">About</a></li>
+                <li><a href="https://discord.gg/f9QTXHt">Discord</a></li>
+                <li><a href="../formats">Formats</a></li>
+                <li><a href="../legal">Legal</a></li>
+                </ul>
+            </nav>
+        </header>
+        	<center>
+        		<br>
+                <img src="../assets/logo.png">
+        	    <h2>Planet Mareep Summary</h2>
+                <div class="box">
+                <p>Planet Mareep is a Pok&eacute;mon-oriented server centered around competitive discussion, impartiality and socialization. Starting as a group for friends then shortly becoming an open-invite server, the server hosts weekly events such as Underrated of The Week and creating unique competitive sets for favorite metagames like Anything Goes and Cross Evolution.
+<br>
+<br>
+The server was created on August 16th 2019 by the top ladder players of Anything Goes and Cross Evolution as a personal server and now having a website and active growing community. The motivation behind the server's creation was the underwhelming amount of good servers hence deciding to make Pok&eacute;mon servers great again.
+<br>
+<br>
+More details coming soon.</p>
+                </div>    
+        	</center>
+        </body>
