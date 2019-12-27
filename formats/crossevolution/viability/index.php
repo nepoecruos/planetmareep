ee26ed6b9c4926743ec2c7dfe599e8a7f7a04b64
Nepo <59269591+nepoecruos@users.noreply.github.com>
2019-12-27 02:49:31 +0000

    Create index.php

---
+++ formats/crossevolution/viability/index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">   
+        <title>Planet Mareep - Cross Evolution</title>
+        <link rel="icon" type="image/PNG" href="../../../assets/logo.png">
+        <link href="../../..//style/about.css" rel="stylesheet" type="text/css">
+        <meta property="og:title" content="Planet Mareep" />
+        <meta property="og:description" content="The #1 Competitive Pok&eacute;mon community." />
+        </head> 
+        <body>
+        <header>
+            <left>
+            <h1><span>Planet Mareep</span></h1></left>
+            <nav>
+                <ul>
+                <li><a href="../../../index.php">Home</a></li>
+                <li><a href="../../../about">About</a></li>
+                <li><a href="https://discord.gg/f9QTXHt">Discord</a></li>
+                <li><a href="../../../formats">Formats</a></li>
+                <li><a href="../../../legal">Legal</a></li>
+                </ul>
+            </nav>
+        </header>
+            <center>
+                <br>
+                <div class="whitebox">
+                <img src="../../../assets/ce2.png" style="width: 50%; height: 50%"/>
+                <p>
+                <a href="https://rom.psim.us/">Play</a>
+                |
+                <a href="../../../formats/crossevolution/updates">Updates</a>
+                |
+                <a href="../../../formats/crossevolution/viability">Viability</a>
+                |
+                <a href="../../../formats/crossevolution/speed">Speed</a></p>
+                </p>
+                <h2>Viability Rankings</h2>
+                <p>
+                    Welcome to the Cross Evolution Viability Rankings. The original one was very inconsistent, inaccurate and had unviable Pok&eacute;mon ranked such as Mega Salamence, Arceus-Fighting, Arceus-Ice, Mega Blaziken, Pheromosa, Tailow, Floette, Salandit, Yanma, Mega Lucario, Doduo, Pancham, Swirlix, Lairon, Sliggoo and Hakamo-o. 
+<br>
+<br>
+They had incorrect order such as Arceus-Fairy being ranked higher than Primal Groudon, Archen being B rank, Pheromosa, Marshadow, Togetic and Fraxure being higher than Kadabra, Krabby, Diglett, Clauncher and Aipom, Type Null being ranked lower than Gligar and more.
+<br>
+<br>
+Overall, the Generation 7 Viability Rankings made by dsm77773 and shrektacularshrek in February of 2017 were the previously mentioned remarks (inconsistent, inaccurate). Now it is even outdated due to the stat restriction unban, previously if a Pokemon's stat went over 255 or under 1 then they are unusable. However, now they just stop at 255 and 1 automatically. So Chansey has access to many more Cross Evolutions, versus the previous rules of Cross Evolution since it's HP was too high only Pok&eacute;mon who gained 5 HP or 0 HP could cross evolve with it.
+<br>
+<br>
+So, perf/snoth also known as the coder of this entire website and author of everything on this site you read has decided to remaster the previous Viability Rankings and make it significantly better since he and Timezone are the new leaders of Cross Evolution.
+<br>
+<br>
+Note: Viability Rankings are in alphabetic order, but donors in parenthesis are in chronological.
+<h2><strong><font color="gold">S</font></strong></h2>
+<img src="../../../assets/icons/gligar.png"/> <span title="(Gyarados, Quagsire, Hariyama, Persian-Alola, Barbaracle)">Gligar</span>
+<br>
+<h2><strong><font color="goldenrod">S-</font></strong></h2>
+<img src="../../../assets/icons/electabuzz.png"/> <span title="(Vivillon, Greninja, Nidoking)">Electabuzz</span>
+<br>
+<img src="../../../assets/icons/magmar.png"/> <span title="(Gallade, Nidoking, Seperior, Clefable)">Magmar</span>
+<br>
+<img src="../../../assets/icons/scyther.png"/> <span title="(Bibarel, Crawdaunt, Breloom, Excadrill)">Scyther</span>
+<br>
+<h2><strong><font color="forestgreen">A+</font></strong></h2>
+<br>
+<img src="../../../assets/icons/chansey.png"/> <span title="(Clefable, Tsareena, Decidueye, Walrein, Chandelure, Incineroar)">Chansey</span>
+<br>
+<img src="../../../assets/icons/munchlax.png"/> <span title="(Milotic, Toxapex, Quagsire, Vespiquen, Xatu)">Munchlax</span>
+<br>
+<img src="../../../assets/icons/rhydon.png"/> <span title="(Gallade, Dragonite, Beedrill, Salamence)">Rhydon</span>
+<br>
+<h2><strong><font color="limegreen">A</font></strong></h2>
+<img src="../../../assets/icons/archen.png"/> <span title="(Hitmonlee, Whimsicott)">Archen</span>
+<br>
+<img src="../../../assets/icons/ferroseed.png"/> <span title="- Ferroseed (Chansey, Pelipper, Hitmontop, Avalugg)">Ferroseed</span>
+<br>
+<img src="../../../assets/icons/magneton.png"/> <span title="(Flygon, Vikavolt, Hydreigon)">Magneton</span>
+<br>
+<img src="../../../assets/icons/rayquaza-mega.png"/> Rayquaza
+<br>
+<img src="../../../assets/icons/spritzee.png"/> <span title="(Milotic, Chansey)">Spritzee</span>
+<br>
+<h2><strong><font color="greenyellow">A-</font></strong></h2>
+<img src="../../../assets/icons/diglett.png"/> <span title="(Espeon, Pangoro, Whimsicott, Kricketune)">Diglett</span>
+<br>
+<img src="../../../assets/icons/doublade.png"/> <span title="(Dragonite, Flygon)">Doublade</span>
+<br>
+<img src="../../../assets/icons/dusclops.png"/> <span title="(Gallade, Incineroar, Clefable, Reuniclus, Slaking)">Dusclops</span>
+<br>
+<img src="../../../assets/icons/gengar-mega.png"/> Gengar-Mega
+<br>
+<img src="../../../assets/icons/groudon-primal.png"/> Groudon-Primal
+<br>
+<img src="../../../assets/icons/haunter.png"/> <span title="(Vivillon, Nidoking, Hydreigon)">Haunter</span>
+<br>
+<img src="../../../assets/icons/porygon2.png"/> <span title="(Gardevoir, Toucannon, Nidoking)">Porygon2</span>
+<br>
+<img src="../../../assets/icons/murkrow.png"/> <span title="(Gyarados, Breloom, Lycanroc-Dusk)">Murkrow</span>
+<br>
+<img src="../../../assets/icons/tangela.png"/> <span title="(Milotic, Chansey, Volcarona)">Tangela</span>
+<br>
+<h2><strong><font color="yellowgreen">B+</font></strong></h2>
+<img src="../../../assets/icons/grimer-alola.png"/> <span title="(Milotic)">Grimer-Alola</span>
+<br>
+<img src="../../../assets/icons/honedge.png"/> <span title="(Hitmontop)">Honedge</span>
+<br>
+<img src="../../../assets/icons/pawniard.png"/> <span title="(Barbaracle)">Pawniard</span>
+<br>
+<img src="../../../assets/icons/klang.png"/> <span title="(Gallade)">Klang</span>
+<br>
+<img src="../../../assets/icons/seadra.png"/> <span title="(Vivillon)">Seadra</span>
+<br>
+<img src="../../../assets/icons/vullaby.png"/> <span title="(Hitmontop)">Vullaby</span>
+<br>
+<img src="../../../assets/icons/xerneas.png"/> Xerneas
+<br>
+<img src="../../../assets/icons/yveltal.png"/> Yveltal
+<br>
+<h2><strong><font color="mediumseagreen">B</font></strong></h2>
+<img src="../../../assets/icons/chinchou.png"/> <span title="(Milotic)">Chinchou</span>
+<br>
+<img src="../../../assets/icons/clauncher.png"/> <span title="(Chansey)">Clauncher</span>
+<br>
+<img src="../../../assets/icons/finneon.png"/> <span title="(Milotic)">Finneon</span>
+<br>
+<img src="../../../assets/icons/frillish.png"/> <span title="(Umbreon, Accelgor)">Frillish</span>
+<br>
+<img src="../../../assets/icons/onix.png"/> <span title="(Vaporeon)">Onix</span>
+<br>
+<img src="../../../assets/icons/kadabra.png"/> <span title="(Nidoking, Porygon-Z, Greninja)">Kadabra</span>
+<br>
+<img src="../../../assets/icons/kyogre-primal.png"/> Kyogre-Primal
+<br>
+<img src="../../../assets/icons/necrozma-dusk-mane.png"> / <img src="../../../assets/icons/necrozma-ultra.png"/> Necrozma
+<br>
+<img src="../../../assets/icons/squirtle.png"/> <span title="(Chansey)">Squirtle</span>
+<br>
+<img src="../../../assets/icons/wailmer.png"/> <span title="(Toxapex)">Wailmer</span>
+<br>
+<h2><strong><font color="#FFFF66">B-</font></strong></h2>
+<img src="../../../assets/icons/abra.png"/> <span title="(Espeon, Accelgor)">Abra</span>
+<br>
+<img src="../../../assets/icons/fraxure.png"/> <span title="(Gallade)">Fraxure</span>
+<br>
+<img src="../../../assets/icons/krabby.png"/> <span title="(Beartic, Ninjask, Araquanid)">Krabby</span>
+<br>
+<img src="../../../assets/icons/kyogre.png"/> Kyogre
+<br>
+<img src="../../../assets/icons/litleo.png"/> <span title="(Noivern)">Litleo</span>
+<br>
+<img src="../../../assets/icons/nosepass.png"/> <span title="(Hippowdon)">Nosepass</span>
+<br>
+<img src="../../../assets/icons/omanyte.png"/> <span title="(Espeon, Swoobat)">Omanyte</span>
+<br>
+<img src="../../../assets/icons/piloswine.png"/> <span title="(Scolipede)">Piloswine</span>
+<br>
+<h2><strong><font color="yellow">C</font></strong></h2>
+<img src="../../../assets/icons/amaura.png"/> <span title="(Jolteon)">Amaura</span>
+<br>
+<img src="../../../assets/icons/arceus-dark.png"/> Arceus-Dark
+<br>
+<img src="../../../assets/icons/arceus-fairy.png"/> Arceus-Fairy
+<br>
+<img src="../../../assets/icons/arceus-ground.png"/> Arceus-Ground
+<br>
+<img src="../../../assets/icons/carvanha.png"/> <span title="(Araquanid)">Carvanha</span>
+<br>
+<img src="../../../assets/icons/krokorok.png"/> <span title="(Scolipede)">Krokorok</span>
+<br>
+<img src="../../../assets/icons/mewtwo.png"/> Mewtwo
+<br>
+                </p>
+                </div>
+                </div>    
+            </center>
+        </body>
