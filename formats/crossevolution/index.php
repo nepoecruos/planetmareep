8d44a34d8c1fda66aa79f43f6ab55756d4222362
Nepo <59269591+nepoecruos@users.noreply.github.com>
2019-12-27 02:48:29 +0000

    Create index.php

---
+++ formats/crossevolution/index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">   
+        <title>Planet Mareep - Cross Evolution</title>
+        <link rel="icon" type="image/PNG" href="../../assets/logo.png">
+        <link href="../../style/about.css" rel="stylesheet" type="text/css">
+        <meta property="og:title" content="Planet Mareep" />
+        <meta property="og:description" content="The #1 Competitive Pok&eacute;mon community." />
+        </head> 
+        <body>
+        <header>
+            <left>
+            <h1><span>Planet Mareep</span></h1></left>
+            <nav>
+                <ul>
+                <li><a href="../../index.php">Home</a></li>
+                <li><a href="../../about">About</a></li>
+                <li><a href="https://discord.gg/f9QTXHt">Discord</a></li>
+                <li><a href="../../formats">Formats</a></li>
+                <li><a href="../../legal">Legal</a></li>
+                </ul>
+            </nav>
+        </header>
+            <center>
+                <br>
+                <div class="whitebox">
+                <img src="../../assets/ce2.png" style="width: 50%; height: 50%"/>
+                <p>
+                <a href="https://rom.psim.us/">Play</a>
+                |
+                <a href="updates">Updates</a>
+                |
+                <a href="viability">Viability</a>
+                |
+                <a href="speed">Speed</a></p>
+                </p>
+                <h2>What is Cross Evolution?</h2>
+                <p>
+                    Since the beginning of the first generation, Pok&eacute;mon had access to evolution which unlocked new stats, moves, typing and eventually abilities. Every Pok&eacute;mon has access to a unique evolution line which allows them to receive their own previously mentioned evolution unlockments. However, what if another Pok&eacute;mon could receive those certain unique unlockments?
+<br>
+<br>
+Here's an example:
+<br>
+                <img src="../../assets/sprites/honedge.gif"/>
+<br>
+Type: <font color="#858799">Steel</font> / <font color="#564A75">Ghost</font>
+<br>
+<br>
+<font color="#858799">Hitmontop (Honedge)</font> @ Life Orb  
+<br>
+Ability: Technician  
+<br>
+EVs: <font color="lime">248 HP</font> / <font color="lime">252 Atk</font> / <font color="red">8 Spe</font> 
+<br>
+Adamant Nature  
+<br>
+<font color="#564A75">- Shadow Sneak</font>  
+<br>
+<font color="#858799">- Bullet Punch</font> 
+<br>
+<font color="#94342B">- Mach Punch</font>  
+<br>
+- Swords Dance
+<br>
+<br>
+HP: <font color="red">45</font> &rarr; <font color="yellow">60</font> (+15)
+<br>
+Attack: <font color="yellow">80</font> &rarr; <font color="lime">140</font> (+60)
+<br>
+Defense: <font color="lime">100</font> &rarr; <font color="lime">160</font> (+60)
+<br>
+Special Attack: <font color="red">35</font> &rarr; <font color="red">35</font> (+0)
+<br>
+Special Defense: <font color="red">37</font> &rarr; <font color="lime">112</font> (+75)
+<br>
+Speed: <font color="red">28</font> &rarr; <font color="yellow">63</font> (+35)
+<br>
+<br>
+BST: <font color="yellow">325</font> &rarr; <font color="lime">570</font> (+245)
+<br>
+<br>
+Honedge now inherits Hitmontop's stat changes, which typically would be applied to Tyrogue. It forgets it's old abilities for Intimidate, Technician and Steadfast which is what Hitmontop gets when it evolves. And it is able to utilize two Hitmontop moves such as Mach Punch and Rapid Spin. (The set above only uses one Hitmontop move)
+<br>
+<br>
+Honedge is able to cross-evolve into Hitmontop because it is a first stage and Hitmontop is a second change. It would be unable to cross-evolve into third stage Pok&eacute;mon because it is a first stage. However, it's second stage Doublade is able to cross-evolve into third stages!
+<br>
+<br>
+Here's an example:
+<br>
+<br>
+<img src="../../assets/sprites/doublade.png"/>
+<br>
+Type: <font color="#858799">Steel</font> / <font color="#7C71AE">Flying</font>
+<br>
+<br>
+<font color="#858799">Dragonite (Doublade)</font> @ Leftovers
+<br>  
+Ability: Multiscale  
+<br>
+EVs: <font color="lime">252 HP</font> / <font color="lime">252 Def</font> / <font color="red">4 SpD</font>
+<br>
+Impish Nature  
+<br>
+<font color="#858799">- Iron Head</font>
+<br>
+<font color="#723F7D">- Toxic</font> 
+<br> 
+<font color="#7C71AE">- Defog</font>  
+<br>
+<font color="#7C71AE">- Roost</font>
+<br>
+<br>
+HP: <font color="red">59</font> &rarr; <font color="lime">89</font> (+30)
+<br>
+Attack: <font color="lime">110</font> &rarr; <font color="lime">160</font> (+50)
+<br>
+Defense: <font color="lime">150</font> &rarr; <font color="lime">180</font> (+30)
+<br>
+Special Attack: <font color="red">45</font> &rarr; <font color="yellow">75</font> (+30)
+<br>
+Special Defense: <font color="red">49</font> &rarr; <font color="yellow">79</font> (+30)
+<br>
+Speed: <font color="red">35</font> &rarr; <font color="red">45</font> (+10)
+<br>
+<br>
+BST: <font color="yellow">448</font> &rarr; <font color="lime">628</font> (+180)
+<br>
+<br>
+Dragonair's second type changes to Flying when it evolves. So whenever another second stage Pok&eacute;mon cross evolves with Dragonite their second typing is changed to Flying automatically. They obtain Inner Focus and Multiscale and 2 Moves from Dragonite. In this case, Doublade inherited Roost and Defog from it. 
+<br>
+<br>
+Doublade cannot cross-evolve into Hitmontop because it is a second stage Pokemon just like Hitmontop. So the rule of the game is, you can cross-evolve with the evolution that's higher than you by one stage. Hence, third stages cannot cross-evolve because fourth stages do not exist. And you are not allowed to cross-evolve into a mega evolution.
+<br>
+<br>
+<img src="../../assets/icons/honedge.png"/> &rarr; <img src="../../assets/icons/hitmontop.png"/> <font color="lime">&#x2714</font>
+<br>
+<img src="../../assets/icons/doublade.png"/> &rarr; <img src="../../assets/icons/dragonite.png"/> <font color="lime">&#x2714</font>
+<br>
+<br>
+<img src="../../assets/icons/doublade.png"/> &rarr; <img src="../../assets/icons/hitmontop.png"/> <font color="red">&#x2718</font>
+<br>
+<img src="../../assets/icons/honedge.png"/> &rarr; <img src="../../assets/icons/dragonite.png"/> <font color="red">&#x2718</font>
+<br>
+<br>
+Additional Information:
+<br>
+<img src="../../assets/icons/doublade.png"/> &rarr; <img src="../../assets/icons/dragonite.png"/> <font color="lime">&#x2714</font>
+<br>
+<img src="../../assets/icons/gliscor.png"/> &rarr; <img src="../../assets/icons/dragonite.png"/> <font color="red">&#x2718</font>
+<br>
+- Second stage Pokemon who do not evolve cannot cross-evolve, for example Doublade can cross-evolve but Gliscor cannot.
+<br>
+- Items like Light Ball or Thick Club will only work on regular Pikachu and Marowak, not cross-evolved ones.
+<br>
+- Eviolite (<img src="../../assets/items/eviolite.png"/>) does not work on cross-evolved Pokemon as they're treated as different and fully evolved species.
+<br>
+- Signature Z-Moves like Extreme Evoboost will not work if your Eevee is cross-evolved.
+                </p>
+                </div>
+                </div>    
+            </center>
+        </body>
