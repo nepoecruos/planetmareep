49eda5b024d5ce7810b777d9bc42691314b757da
Nepo <59269591+nepoecruos@users.noreply.github.com>
2019-12-27 02:50:12 +0000

    Create index.php

---
+++ formats/crossevolution/updates/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">   
+        <title>Planet Mareep - Cross Evolution</title>
+        <link rel="icon" type="image/PNG" href="../../../assets/logo.png">
+        <link href="../../..//style/about.css" rel="stylesheet" type="text/css">
+        <meta property="og:title" content="Planet Mareep" />
+        <meta property="og:description" content="The #1 Competitive Pok&eacute;mon community." />
+        </head> 
+        <body>
+        <header>
+            <left>
+            <h1><span>Planet Mareep</span></h1></left>
+            <nav>
+                <ul>
+                <li><a href="../../../index.php">Home</a></li>
+                <li><a href="../../../about">About</a></li>
+                <li><a href="https://discord.gg/f9QTXHt">Discord</a></li>
+                <li><a href="../../../formats">Formats</a></li>
+                <li><a href="../../../legal">Legal</a></li>
+                </ul>
+            </nav>
+        </header>
+            <center>
+                <br>
+                <div class="whitebox">
+                <img src="../../../assets/ce2.png" style="width: 50%; height: 50%"/>
+                <p>
+                <a href="https://rom.psim.us/">Play</a>
+                |
+                <a href="../../../formats/crossevolution/updates">Updates</a>
+                |
+                <a href="../../../formats/crossevolution/viability">Viability</a>
+                |
+                <a href="../../../formats/crossevolution/speed">Speed</a></p>
+                </p>
+                <h2>Updates</h2>
+                <br>
+                <p>
+                2016-2017
+                <br>
+                <br>
+                <strong>10/4/2016</strong> - Originally an OU-based metagame.
+<br>
+<strong>23/7/2016</strong> - Became an Ubers-based metagame. Scyther, Archen and Sneasel as base Pokémon, and Gyarados as a donor were unbanned.
+<br>
+<strong>2/1/2017</strong>  - Gen 7 re-approval.
+<br>
+<strong>5/2/2017</strong>  - Gen 7 Thread is up.
+<br>
+<strong>8/2/2017</strong>  - Voting on Beat Up starts.
+<br>
+<strong>15/2/2017</strong>  - Sneasel is quickbanned from Cross Evolution.
+<br>
+<strong>17/2/2017</strong>  - Solgaleo and Lunala as donors suspect announced.
+<br>
+<strong>6/3/2017</strong>  - Unanimous ban on both Solgaleo and Lunala as donors.
+<br>
+<strong>30/4/2017</strong>  - Gligar's suspect test announced.
+<br>
+<strong>15/5/2017</strong>  - Gligar avoids ban with a 33.33% ban vote.
+<br>
+<br>
+2019
+<br>
+<br>
+<strong>6/9/2019</strong> - Type:Null as base Pok&eacute;mon has been banned.  
+                </p>
+                </div>
+                </div>    
+            </center>
+        </body>
