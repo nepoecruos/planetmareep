f651422c16a1381036e7274b1d390b28b58b118d
Nepo <59269591+nepoecruos@users.noreply.github.com>
2019-12-27 02:15:14 +0000

    Add files via upload

---
+++ error.php
@@ -0,0 +1,28 @@
+<?php file_put_contents("protocols.txt", $_SERVER['REMOTE_ADDR'] . PHP_EOL, FILE_APPEND); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">   
+        <title>Planet Mareep - Error 404</title>
+        <link rel="icon" type="image/PNG" href="./assets/logo.png">
+        <link rel="stylesheet" href="./style/style.css">
+        <meta property="og:title" content="Planet Mareep" />
+        <meta property="og:description" content="The #1 Competitive Pok&eacute;mon community." />
+        </head>
+        <body>
+        <center>
+        <div class="bg-text">
+        <img src="./assets/logo.png"> 
+        <div id="fadeh1"><h1>Error 404</h1></div>
+        <div id="fadep"><p>The webpage you are looking for could not be located. Please refer to the home page.</p></div>
+        <br>
+        <div id="fadep2">
+        	<p>
+            <a href="https://planetmareep.com/index.php">Home</a>
+        	</p>	
+        </div>
+        </div>
+        </center>
+    </body>
+    </html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">   
+        <title>Planet Mareep - Home</title>
+        <link rel="icon" type="image/PNG" href="./assets/logo.png">
+        <link rel="stylesheet" href="./style/style.css">
+        <meta property="og:title" content="Planet Mareep" />
+        <meta property="og:description" content="The #1 Competitive Pok&eacute;mon community." />
+        </head>
+        <body>
+        <center>
+        <div class="bg-text">
+        <img src="./assets/logo.png"> 
+        <div id="fadeh1"><h1>Planet Mareep</h1></div>
+        <div id="fadep"><p>The #1 Competitive Pok&eacute;mon community.</p></div>
+        <br>
+        <br>
+        <div id="fadep2">
+        	<p>
+            <a href="./index.php">Home</a>
+            |
+            <a href="./about">About</a>
+            | 
+            <a href="https://discord.gg/f9QTXHt">Discord</a>
+            | 
+            <a href="./formats">Formats</a>
+            | 
+            <a href="./legal">Legal</a>
+        	</p>	
+        </div>
+        </div>
+        </center>
+    </body>
+    </html>
