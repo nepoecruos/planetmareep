9aeeeedf9f7a710c4af6e4b0d33252e8c86212fd
Nepo <59269591+nepoecruos@users.noreply.github.com>
2019-12-27 02:47:32 +0000

    Create index.php

---
+++ formats/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">   
+        <title>Planet Mareep - Formats</title>
+        <link rel="icon" type="image/PNG" href="../assets/logo.png">
+        <link href="../style/about.css" rel="stylesheet" type="text/css">
+        <meta property="og:title" content="Planet Mareep" />
+        <meta property="og:description" content="The #1 Competitive Pok&eacute;mon community." />
+        </head>	
+        <body>
+        <header>
+            <left>
+            <h1><span>Planet Mareep</span></h1></left>
+            <nav>
+                <ul>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="../about">About</a></li>
+                <li><a href="https://discord.gg/f9QTXHt">Discord</a></li>
+                <li><a href="../formats">Formats</a></li>
+                <li><a href="../legal">Legal</a></li>
+                </ul>
+            </nav>
+        </header>
+        	<center>
+        		<br>
+                <img src="../assets/formats.png">
+                 <table>
+                    <td><div class="gallery">
+                            <a target="_blank" href="ag">
+                              <img src="../assets/ag.png">
+                            </a>
+                            <div class="desc">Anything Goes</div>
+                          </div></td>
+                          <td><div class="gallery">
+                                <a target="_blank" href="../formats/crossevolution/">
+                                  <img src="../assets/ce.png">
+                                </a>
+                                <div class="desc">Cross Evolution</div>
+                              </div></td>  
+                        </table> 
+                </div>    
+        	</center>
+        </body>
